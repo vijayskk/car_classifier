e8482546dd27005376ec3770fac10c6cf84c2ef3
vijayskk <vijayskk007@gmail.com>
2022-03-17 12:23:46 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import sklearn 
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+
+
+data = pd.read_csv('car.data')
+
+print(data)
